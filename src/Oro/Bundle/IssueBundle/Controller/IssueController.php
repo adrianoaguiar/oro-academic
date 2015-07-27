bb2fde7aba3dc44f27cecc9eadd2d92dd76b1772
Ievgenii Myshusta <imyshusta@magecore.com>
2015-07-27 14:06:40 +0300

    OOT-764 - Issues Grid

--- src/Oro/Bundle/IssueBundle/Controller/IssueController.php
+++ src/Oro/Bundle/IssueBundle/Controller/IssueController.php
@@ -13,12 +13,6 @@ class IssueController extends Controller
 {
     /**
      * @Route(name="oro_issue_index")
-     * @Acl(
-     *      id="oro_issue_view",
-     *      type="entity",
-     *      class="OroIssueBundle:Issue",
-     *      permission="VIEW"
-     * )
      * @Template()
      */
     public function indexAction()
@@ -26,5 +20,47 @@ class IssueController extends Controller
         return array(
             'entity_class' => $this->container->getParameter('oro_issue.entity.issue.class')
         );
+    }
+
+    /**
+     * @Route("/view/{id}", name="oro_issue_view", requirements={"id"="\d+"})
+     * @Template
+     *
+     * @param Issue $issue
+     * @return array
+     */
+    public function viewAction(Issue $issue)
+    {
+        return array('entity' => $issue);
+    }
+
+    /**
+     * @Route("/create", name="oro_issue_create")
+     * @Template("OroIssueBundle:Issue:update.html.twig")
+     */
+    public function createAction()
+    {
+
+    }
+
+    /**
+     * @Route("/update/{id}", name="oro_issue_update", requirements={"id"="\d+"})
+     * @Template()
+     *
+     * @param Issue $entity
+     * @return array
+     */
+    public function updateAction(Issue $entity)
+    {
+
+    }
+
+    /**
+     * @Route("/delete/{id}", name="oro_issue_delete", requirements={"id"="\d+"})
+     * @Template()
+     */
+    public function deleteAction($id)
+    {
+
     }
 }
